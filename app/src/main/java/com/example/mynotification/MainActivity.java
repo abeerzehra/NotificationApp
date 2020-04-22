9575dfffaa977d2e53079f6f0a66ce8a227faeae
abeerzehra <k164068@nu.edu.pk>
2020-04-22 14:35:02 +0500

    qeds

--- app/src/main/java/com/example/mynotification/MainActivity.java
+++ app/src/main/java/com/example/mynotification/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.mynotification;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    NotificationCounter notificationCounter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        notificationCounter=new NotificationCounter(findViewById(R.id.bell));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationCounter.IncreaseNumber();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mynotification/NotificationCounter.java
+++ app/src/main/java/com/example/mynotification/NotificationCounter.java
@@ -1,4 +1,27 @@
 package com.example.mynotification;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 public class NotificationCounter {
+    private TextView notificationNumber;
+
+    private final int MAX_NUMBER=99;
+    private  int notification_number_counter = 1;
+
+    public NotificationCounter(View view){
+        notificationNumber = view.findViewById(R.id.notificationNumber);
+    }
+
+    public void  IncreaseNumber(){
+        notification_number_counter++;
+
+        if(notification_number_counter>MAX_NUMBER){
+            Log.d("counter","Max number reached");
+        }else{
+                notificationNumber.setText(String.valueOf(notification_number_counter));
+        }
+    }
 }
+
